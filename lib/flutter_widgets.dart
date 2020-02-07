1fe0a77219c91d00f489a0a40adb96c72807fa72
jamesdlin <jamesdlin@google.com>
2020-02-07 11:36:32 -0800

    Properly export responsive_ui and feature_discovery
    flutter_widgets 0.1.7 published responsive_ui and feature_discovery,
    but we forgot to export them from the main flutter_widgets.dart file.
    
    Fixes https://github.com/google/flutter.widgets/issues/63.
    
    PiperOrigin-RevId: 293859022

--- lib/flutter_widgets.dart
+++ lib/flutter_widgets.dart
@@ -4,8 +4,10 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
+export 'src/feature_discovery/feature_discovery.dart';
 export 'src/html_widget/html_widget.dart';
 export 'src/linked_scroll_controller/linked_scroll_controller.dart';
+export 'src/responsive_ui/responsive_ui.dart';
 export 'src/scrollable_positioned_list/scrollable_positioned_list.dart';
 export 'src/tagged_text/tagged_text.dart';
 export 'src/visibility_detector/visibility_detector.dart';
