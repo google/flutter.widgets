fc19e0a122132db23740651cd329705f8e55470d
Mehmet Fidanboylu <mehmetf@google.com>
2020-03-13 08:30:34 -0700

    Fix tools script

--- tool/travis.sh
+++ tool/travis.sh
@@ -27,8 +27,8 @@ set -e
 
 # Run the tests.
 echo "Running tests in each package..."
-for package in packages/*/; do
-  pushd "packages/$package"
+for package in packages/*; do
+  pushd "$package"
   flutter test
   popd
 done
