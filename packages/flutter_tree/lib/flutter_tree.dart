011c51c8b343cc915c3651d8bfd4820abd967b07
Polina Cherkasova <polina.c@live.com>
2020-05-19 13:02:39 -0700

    Delete flutter_tree.dart

--- packages/flutter_tree/lib/flutter_tree.dart
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 the Dart project authors.
-//
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file or at
-// https://developers.google.com/open-source/licenses/bsd
-
-export 'src/tree_node.dart';
-export 'src/flutter_tree.dart';
