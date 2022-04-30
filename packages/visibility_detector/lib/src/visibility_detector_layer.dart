576766fc0ac7604bd7870d0d1cd5680d8a92a6dd
Alexey Inkin <leha@inkin.ru>
2022-04-30 18:04:31 +0400

    Fix null checks and assertions for visibility_detector (#354)

--- packages/visibility_detector/lib/src/visibility_detector_layer.dart
+++ packages/visibility_detector/lib/src/visibility_detector_layer.dart
@@ -123,8 +123,8 @@ class VisibilityDetectorLayer extends ContainerLayer {
 
   /// Computes the accumulated clipping bounds, in global coordinates.
   Rect _computeClipRect() {
-    assert(RendererBinding.instance.renderView != null);
-    var clipRect = Offset.zero & RendererBinding.instance.renderView.size;
+    assert(RendererBinding.instance?.renderView != null);
+    var clipRect = Offset.zero & RendererBinding.instance!.renderView.size;
 
     var parentLayer = parent;
     while (parentLayer != null) {
@@ -167,7 +167,7 @@ class VisibilityDetectorLayer extends ContainerLayer {
       if (isFirstUpdate) {
         // We're about to render a frame, so a post-frame callback is guaranteed
         // to fire and will give us the better immediacy than `scheduleTask<T>`.
-        SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+        SchedulerBinding.instance!.addPostFrameCallback((timeStamp) {
           _processCallbacks();
         });
       }
@@ -190,7 +190,7 @@ class VisibilityDetectorLayer extends ContainerLayer {
     // of `addPostFrameCallback` or `scheduleFrameCallback` so that work will
     // be done even if a new frame isn't scheduled and without unnecessarily
     // scheduling a new frame.
-    SchedulerBinding.instance
+    SchedulerBinding.instance!
         .scheduleTask<void>(_processCallbacks, Priority.touch);
   }
 
