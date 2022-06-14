cb16665e740790c654c79c6ab7e9eb25b67abaf0
Michael Thomsen <mit@google.com>
2022-06-13 22:08:36 -0400

    Fix CI

--- packages/visibility_detector/example/lib/main.dart
+++ packages/visibility_detector/example/lib/main.dart
@@ -529,7 +529,7 @@ class RowColumn extends Comparable<RowColumn> {
   }
 
   @override
-  int get hashCode => hashValues(row, column);
+  int get hashCode => Object.hash(row, column);
 
   /// See [Comparable.compareTo].  Sorts [RowColumn] objects in row-major order.
   @override
